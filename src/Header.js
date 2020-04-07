dfadf7b4bf3e686563c55dc96c4e9ee6b6b609ee
unknown <garvitsaxena06@gmail.com>
2020-04-07 17:47:35 +0530

    commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./header.css"
+
+function Header() {
+    return (
+        <header className="header">
+            <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface-PNG.png" width="200px" alt="troll-face"/>
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
