eeecec7f77b7a061b928368ebdfa0cdb7bf89c07
unknown <garvitsaxena06@gmail.com>
2020-04-07 17:48:08 +0530

    Updates

---
+++ precache-manifest.7ba48666fdbdc757d2600327ad7ea233.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da36e1dc1ada1fc87c3347854ad6ba87",
+    "url": "/Meme-generator/index.html"
+  },
+  {
+    "revision": "fc194985c40c0142a237",
+    "url": "/Meme-generator/static/css/main.4b836e6a.chunk.css"
+  },
+  {
+    "revision": "a8c749a33d9bac01b08f",
+    "url": "/Meme-generator/static/js/2.b3b313ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Meme-generator/static/js/2.b3b313ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc194985c40c0142a237",
+    "url": "/Meme-generator/static/js/main.faf6480f.chunk.js"
+  },
+  {
+    "revision": "6b514ce40a65b00dc308",
+    "url": "/Meme-generator/static/js/runtime-main.9b48adb2.js"
+  }
+]);
